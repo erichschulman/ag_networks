35e9c712174b9ee2e2ce5f3f1623ea1487f54339
Eric Schulman <erichschulman@localhost.localdomain>
2017-02-07 23:18:26 -0500

    cleaned up the ocode a little

--- edges.py
+++ edges.py
@@ -6,6 +6,7 @@ import json
 #to start osrm osrm-routed ../maps/new-york-latest.osrm from the directory
 
 def ps_edges(db):
+	"""computes edge weights between stores and processors"""
 	conn = sqlite3.connect(db)
 	c = conn.cursor()
 
@@ -18,13 +19,19 @@ def ps_edges(db):
 		for procid in range(1,procnum+1): #may change this depending on RAM constraints
 			c.execute("SELECT proc.procid, proc.lat, proc.lon, stores.storeid, stores.lat, stores.lon FROM proc, stores WHERE proc.procid=? AND stores.storeid=?",(procid,storeid,))
 			row = c.fetchone()
-			req = "http://0.0.0.0:5000/route/v1/table/%s,%s;%s,%s"%(row[2],row[1],row[5],row[4])
-			osrm_raw = requests.get(req)
-			orsm =json.loads(osrm_raw.text)
-			duration = orsm['routes'][0]['duration']
+			duration = routing(row[2],row[1],row[5],row[4])
 			c.execute('INSERT INTO ps_edges VALUES (?,?,?,?)', (storeid,procid,0,duration,))
 	conn.commit()
 	return
+
+
+def routing(lon0,lat0,lon1,lat1):
+	"""wrapper for doing routing with OSRM"""
+	req = "http://0.0.0.0:5000/route/v1/table/%s,%s;%s,%s"%(lon0,lat0,lon1,lat1)
+	osrm_raw = requests.get(req)
+	orsm =json.loads(osrm_raw.text)
+	return orsm['routes'][0]['duration']
+
 
 
 if __name__ == "__main__":
