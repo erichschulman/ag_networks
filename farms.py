aa715b038452d448734bda160f55b1aa9c1ea0bf
Eric Schulman <erichschulman@localhost.localdomain>
2017-02-15 17:38:50 -0500

    got the sieve working. now to get attributes working and importing to the db

--- farms.py
+++ farms.py
@@ -1,11 +1,11 @@
 import sqlite3
 import numpy as np
-from osgeo import gdal, gdalconst, gdalnumeric, ogr
+from osgeo import gdal, gdalconst, gdalnumeric, ogr, osr
 import sys
 import os
 
 
-def import_farms(file_name, band, seive):
+def import_farms(file_name, band, sieve):
 	gdal.UseExceptions()
 
 	#create a folder for the band output
@@ -27,27 +27,39 @@ def import_farms(file_name, band, seive):
 	file2 = driver1.Create(folder + "/band_" + str(band) + ".tiff", file1.RasterXSize, file1.RasterYSize, 1, band1.DataType)
 	gdalnumeric.CopyDatasetInfo(file1,file2)
 	band2 = file2.GetRasterBand(1)
-	band2.SetNoDataValue(0)
 	gdalnumeric.BandWriteArray(band2, data2)
+
+	gdal.SieveFilter(band2, None, band2, sieve, callback=None) #then sieve the data
+	band2.SetNoDataValue(0)
 
 	#close original data
 	file1 = None
 	band1 = None
 
+	
+
 	#then polygonize
+	prj2 = file2.GetProjection()
+	srs2=osr.SpatialReference(wkt=prj2) #adding a coordinate system the shapefile
+
 	driver2 = ogr.GetDriverByName("ESRI Shapefile")
 	file3 = driver2.CreateDataSource(folder + "/band_" + str(band) + ".shp" )
-	data3 = file3.CreateLayer( "band_" + str(band) , srs = None )
+	data3 = file3.CreateLayer( "band_" + str(band) , srs = srs2)
 	gdal.Polygonize( band2, band2, data3, -1, [], callback=None )
 
-	#then sieve the data
+	#close the files
+	file2 = None
+	band2 = None
 
+	
 	#then calculate fields
 
+
 	#then move fields to db
+
 
 	#close dataset
 	return
 
 if __name__ == "__main__":
-	import_farms('input/test.tif',36,10000)
+	import_farms('input/test.tif',36,10)
