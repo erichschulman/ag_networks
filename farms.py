c46e2a0b9bc381293d7224436f0174a825ab08f4
Eric Schulman <erichschulman@localhost.localdomain>
2017-02-15 14:46:21 -0500

    figured out how to do band algebra in python

--- farms.py
+++ farms.py
@@ -1,7 +1,8 @@
 import sqlite3
-from osgeo import gdal, ogr
+import numpy as np
+from osgeo import gdal, gdalconst, gdalnumeric
 import sys
-
+import os
 
 def test():
 	conn1 = sqlite3.connect('db/test.db', isolation_level = 'DEFERRED')
@@ -22,14 +23,47 @@ def test2():
 		print(row)
 
 
-def import_farms(file, band):
+def import_farms(file_name, band, seive):
 	gdal.UseExceptions()
 
-	ds = gdal.Open('test.tif')
-	srcband = src_ds.GetRasterBand(band)
+	#create a folder for the band output
+	folder = "output/band_" + str(band)
+	if not os.path.exists(folder):
+		os.makedirs(folder)
+
+    #open file
+	file1 = gdal.Open(file_name)
+	band1 = file1.GetRasterBand(1)
+	data1 = gdalnumeric.BandReadAsArray(band1)
+
+	#limit band to the data i care about
+	rastercalc = np.vectorize(lambda x: 1 if x==band else None)
+	data2 = rastercalc(data1)
+
+	#create a new geotiff
+	driver1 = gdal.GetDriverByName("GTiff")
+	file2 = driver1.Create(folder + "/band_" + str(band) + ".tiff", file1.RasterXSize, file1.RasterYSize, 1, band1.DataType)
+	gdalnumeric.CopyDatasetInfo(file1,file2)
+	band2 = file2.GetRasterBand(1)
+	gdalnumeric.BandWriteArray(band2, data2)
+
+	#convert to no data
+
+	#close original data
+	file1 = None
+	band1 = None
+	file2 = None
+	band2 = None
+
+
+	#then polygonize
+
+	#then sieve the data
+
+	#then calculate fields
 
 	#close dataset
-	ds = None
+	return
 
 if __name__ == "__main__":
-	import_farms('input/NASSnyc2010.036.tif.8033/cdl_tm_r_ny_2010_utm18.tif',68):
+	import_farms('input/test.tif',36,10000)
