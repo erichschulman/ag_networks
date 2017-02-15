7adf78a4d57041cbb03b21a985792ad6d6c94c49
Eric Schulman <erichschulman@localhost.localdomain>
2017-02-15 14:46:53 -0500

    deleted random tests

--- farms.py
+++ farms.py
@@ -4,24 +4,6 @@ from osgeo import gdal, gdalconst, gdalnumeric
 import sys
 import os
 
-def test():
-	conn1 = sqlite3.connect('db/test.db', isolation_level = 'DEFERRED')
-	conn2 = sqlite3.connect('db/test.db', isolation_level = 'DEFERRED')
-	c1 = conn1.cursor()
-	c2 = conn2.cursor()
-	i = 1
-	for row in c2.execute('SELECT * FROM proc'):
-		print(row)
-		i = i+1
-		c1.execute('INSERT INTO stores VALUES (?,?,?,?)', (20+i,20,20,20,) )
-	conn1.commit()
-
-def test2():
-	conn1 = sqlite3.connect('db/test.db', isolation_level = 'DEFERRED')
-	c1 = conn1.cursor()
-	for row in c1.execute('SELECT * FROM ?', 'stores'):
-		print(row)
-
 
 def import_farms(file_name, band, seive):
 	gdal.UseExceptions()
