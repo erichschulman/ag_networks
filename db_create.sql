874b08dab68caa1ed7f3e0dfbd978321817d4ea8
Eric Schulman <erichschulman@localhost.localdomain>
2017-02-17 11:19:45 -0500

    added views to the table, switching to laptop to adjust the optimizer

--- db_create.sql
+++ db_create.sql
@@ -1,5 +1,5 @@
 
-/*associate bands with names using input/band.txt*/
+/*Associate bands with names using input/band.txt*/
 CREATE TABLE bands (
 	band INTEGER,
 	name TEXT,
@@ -59,6 +59,7 @@ CREATE TABLE fp_edges (
 	PRIMARY KEY(farmid, procid)
 );
 
+
 /*store processor edges*/
 CREATE TABLE ps_edges (
 	storeid	INTEGER,
@@ -68,3 +69,45 @@ CREATE TABLE ps_edges (
 	FOREIGN KEY (procid) REFERENCES proc(procid)
 	PRIMARY KEY(storeid, procid)
 );
+
+
+/*this query returns farms and their area AS a percentage of the total*/
+CREATE VIEW farm_percents AS
+SELECT farmid, (100*area/tot) AS percent FROM
+farms, (SELECT CAST(SUM(area) AS FLOAT) AS tot FROM farms);
+
+
+/*this query returns stores and their area*census value (by median property value) AS percent of total*/
+CREATE VIEW store_percents AS
+SELECT storeid, (100*value*sqftg/tot) AS percent
+FROM
+(SELECT * FROM stores AS S, tractvalues AS T
+WHERE T.geoid = S.geoid),
+(SELECT CAST(sum(value*sqftg) AS FLOAT) AS tot
+FROM stores AS S, tractvalues AS T);
+
+
+/*this query finds the min, dist between farms without proc id*/
+CREATE VIEW fs_edges AS 
+SELECT fp_edges.farmid AS farmid, ps_edges.storeid AS storeid,
+MIN(fp_edges.routdist + ps_edges.routdist) AS dist
+FROM ps_edges, fp_edges
+WHERE ps_edges.procid = fp_edges.procid
+GROUP BY fp_edges.farmid, ps_edges.storeid;
+
+
+/*this query finds the min, dist between farms including proc id*/
+CREATE VIEW fps_edges AS 
+SELECT  A.storeid, A.farmid, B.procid, B.fp_dist, B.ps_dist
+FROM
+(SELECT fp_edges.farmid AS farmid, ps_edges.storeid AS storeid,
+MIN(fp_edges.routdist + ps_edges.routdist) AS dist
+FROM ps_edges, fp_edges
+WHERE ps_edges.procid = fp_edges.procid
+GROUP BY fp_edges.farmid, ps_edges.storeid) AS A,
+(SELECT fp_edges.farmid AS farmid, ps_edges.storeid AS storeid, 
+fp_edges.procid AS procid, fp_edges.routdist + ps_edges.routdist AS dist,
+fp_edges.routdist AS fp_dist, ps_edges.routdist AS ps_dist
+FROM ps_edges, fp_edges
+WHERE ps_edges.procid = fp_edges.procid) AS B
+WHERE A.storeid = B.storeid AND A.farmid = B.farmid AND A.dist = B.dist;
